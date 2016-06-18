8e17dbd5f1e10732f28a58294074c9fde657aa40
phonofidelic <chris_clemons85@hotmail.com>
2016-06-18 14:52:33 -0400

    Got rid of passw strenth plugins

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,18 +2,77 @@
 'use-strict';
 angular.module('signUp', []);
 
-angular.module('signUp').controller('SignUppController', ['$scope', '$log', function($scope, $log) {
-	var data = {},
-		inputArr = [],
+angular.module('signUp').controller('SignUppController', ['$scope', '$log', 'IssueTracker', function($scope, $log, IssueTracker) {
+	var inputArr = [],
 		vm = this;
+	vm.data = {};
+
+	var issueTracker = new IssueTracker();
 
 	vm.checkValidation = function() {
-		// collect all input values
-		angular.element('input.form-control').each(function() {
-			var input = angular.element(vm);
-			inputArr.push(input[0]);
-		});
+		// check name input
+		if (vm.data.name === undefined) {
+			issueTracker.add('Pleas enter your name.');
+		}
 
-		$log.log('inputArr: ', inputArr);
+		// check email input
+		if (vm.data.email === undefined) {
+			issueTracker.add('Please enter a vallid email address.');
+		}
+
+		// check password
+		if (vm.data.password1 === undefined) {
+			issueTracker.add('Pleas create a password.')
+		}
+
+		// check that passwords match
+
+		var inputIssues = issueTracker.retrieve();
+
+		$log.log('data: ', vm.data);
+		$log.log(inputIssues);
+
+	};
+
+	// check password strength
+	vm.passwordStrength = function() {
+		var password = vm.data.password1.split('');
+		var strength = [];
+		// var reccomendedChars = [/[A-z0-9\!\@\#\$\%\^\&\*]/g];
+		var reccomendedChars = ['!', '@', '#', '$', '%', '^', '&', '*'];
+		for (var i = 0; i < password.length; i++) {
+			$log.log('password indexOf: ', password.indexOf(reccomendedChars[i]));
+			var found = password.indexOf(reccomendedChars[i]);
+			if (found != -1) {
+				strength.push(found);
+			}
+		}
+		$log.log('found: ', strength);
 	};
 }]);
+
+angular.module('signUp').factory('IssueTracker', function() {
+	function IssueTracker() {
+		this.issues = [];
+	};
+	IssueTracker.prototype = {
+		add: function(issue) {
+			this.issues.push(issue);
+		},
+		retrieve: function() {
+			var message = '';
+			switch (this.issues.length) {
+			case 0:
+				break;
+			case 1: 
+				message = 'Please fix the following issue:\n' + this.issues[0];
+				break;
+			case 2:
+				message = 'Please fix the following issues:\n' + this.issues.join('\n');
+				break;
+			}
+			return message;
+		}
+	};
+	return IssueTracker;
+});
