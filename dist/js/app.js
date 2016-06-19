fb5268d9ebbb935a385a79de919a2aa05145aaea
phonofidelic <chris_clemons85@hotmail.com>
2016-06-19 01:21:11 -0400

    Added sendData method to vm. Trying out local storage.

--- dist/js/app.js
+++ dist/js/app.js
@@ -20,26 +20,36 @@ angular.module('signUp').controller('SignUppController', ['$scope', '$log', 'Iss
 			issueTracker.add('Please enter a vallid email address.');
 		}
 
-		// // check password
-		// if (vm.data.password1 === "") {
-		// 	issueTracker.add('Pleas create a password.');
-		// }
+		// check password
+		if (angular.isUndefined(vm.data.password1)) {
+			issueTracker.add('Pleas create a password.');
+		}
 
 		// check that passwords match
+		if (vm.data.password1 !== vm.data.password2) {
+			issueTracker.add('Passwords do not match.');
+		}
 
 		var inputIssues = issueTracker.retrieve();
 
 		$log.log('data: ', vm.data);
 		$log.log(inputIssues);
 
+		// after all inputs pass, send user-data to back-end (or 
+		// whatever we're using as a back-end)
+		vm.sendData(vm.data);
+	};
+
+	vm.sendData = function(data) {
+		localStorage.setItem('user_name', data.name);
+		localStorage.setItem('user_email', data.email);
 	};
 
 	// check password strength
 	var passwordStrengthMeter = new PasswordStrengthMeter();
 	vm.passwordStrength = function(password) {
-		vm.passwordProgress = passwordStrengthMeter.passTest(password);
-		
-		
+		vm.passwordProgress = passwordStrengthMeter.passTest(password);	
+		// send passwordProgress value for ng-style to set width of progress bar
 		vm.passwordProgress = vm.passwordProgress+'%';
 		$log.log('vm.passwordProgress: ', vm.passwordProgress);
 		return vm.passwordProgress;
@@ -109,7 +119,7 @@ angular.module('signUp').factory('IssueTracker', function() {
 			case 1: 
 				message = 'Please fix the following issue:\n' + this.issues[0];
 				break;
-			case 2:
+			default:
 				message = 'Please fix the following issues:\n' + this.issues.join('\n');
 				break;
 			}
