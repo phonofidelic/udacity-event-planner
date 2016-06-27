30e139581b9804932fec4683970fd107c5f051d8
phonofidelic <chris_clemons85@hotmail.com>
2016-06-27 12:34:15 -0400

    Started on event creation section. Added bootstrap-select plugin.

---
+++ dist/js/controllers/CreateEventController.js
@@ -0,0 +1,7 @@
+/*eslint angular/di: [2,"array"]*/
+'use-strict';
+angular.module('eventPlan').controller('CreateEventController', ['$scope', '$log', '$firebaseObject', function($scope, $log, $firebaseObject) {
+	var vm = this;
+
+	vm.eventData = {};
+}]);
