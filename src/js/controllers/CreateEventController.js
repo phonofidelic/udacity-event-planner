73757b7721b6040bcf2c91eb9b57079e972d685f
phonofidelic <chris.clemons85@gmail.com>
2016-07-04 02:01:47 -0400

    Changed jquery version to be compatible with bootstrap.

--- src/js/controllers/CreateEventController.js
+++ src/js/controllers/CreateEventController.js
@@ -9,8 +9,8 @@ angular.module('eventPlan').controller('CreateEventController', ['$scope', '$log
 
 	vm.eventData = {};
 
-    // set event uid property 
-    vm.eventData.eventCreatorsUid = firebase.auth().currentUser.uid;
+	// set event uid property 
+	vm.eventData.eventCreatorsUid = firebase.auth().currentUser.uid;
 
 	dbObject.$loaded()
 		.then(function(data) {
@@ -20,20 +20,20 @@ angular.module('eventPlan').controller('CreateEventController', ['$scope', '$log
 			$log.log('syncObject error: ', error);
 		});
 
-    vm.autoAddress = function() {
-        var defaultBounds = new google.maps.LatLngBounds(
-          new google.maps.LatLng(-33.8902, 151.1759),
-          new google.maps.LatLng(-33.8474, 151.2631));
-        var input = document.getElementById('inp-event-location');
-        var options = {
-            // bounds: defaultBounds,
-            types: ['address']
-        };
-        $scope.addrAoutocomplete = new google.maps.places.Autocomplete(input, options);
-    };
+	vm.autoAddress = function() {
+		var defaultBounds = new google.maps.LatLngBounds(
+		  new google.maps.LatLng(-33.8902, 151.1759),
+		  new google.maps.LatLng(-33.8474, 151.2631));
+		var input = document.getElementById('inp-event-location');
+		var options = {
+			// bounds: defaultBounds,
+			types: ['address']
+		};
+		$scope.addrAoutocomplete = new google.maps.places.Autocomplete(input, options);
+	};
 
-    vm.saveEvent = function() {
-    	firebase.database().ref().child('events/').push(vm.eventData);
-    };
+	vm.saveEvent = function() {
+		firebase.database().ref().child('events/').push(vm.eventData);
+	};
 
 }]);
