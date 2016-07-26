206a7a7c2e56e8267787a0ebbc318579adea2330
phonofidelic <chris.clemons85@gmail.com>
2016-07-25 22:35:25 -0400

    Trying app.use()

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 var express = require('express');
 
 var app = express();
-
-app.get('/', function(req, res) {
-	// app.use(express.static('dist'));
-	res.send('hello world');
-});
+app.use(express.static('dist'));
+// app.get('/', function(req, res) {
+// 	// app.use(express.static('dist'));
+// 	res.send('hello world');
+// });
 
 app.listen(process.env.PORT || 3001, function() {
 	console.log('listening on port 8080');
