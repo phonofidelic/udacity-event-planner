9f7b3d0b5f09dd799e8d8a8fc8d572b5bc6c7fd5
phonofidelic <chris.clemons85@gmail.com>
2016-07-25 21:00:54 -0400

    Testing express route.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('dist'));
+app.get('/', function(req, res) {
+	// app.use(express.static('dist'));
+	res.send('hello world');
+});
 
 app.listen(8080, function() {
 	console.log('listening on port 8080');
